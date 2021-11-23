c2f035d8ee6dd6a0e16f3d1205b576d3c1adb848
Oscarholm <87929189+Oscarholm@users.noreply.github.com>
2021-11-23 19:53:58 +0100

    fully tested in ubuntu

--- setup.sh
+++ setup.sh
@@ -5,13 +5,15 @@
 # Install programs
 sudo apt update
 sudo apt install \
-    curl git vim python-pip \
-    python-virtualenv python-dev \
+    curl git vim python3-pip \
     rsync tmux
 
 # Install nvm: node-version manager
 # https://github.com/nvm-sh/nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install node
 nvm use node
 
